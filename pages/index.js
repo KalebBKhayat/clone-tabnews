369999e85a726ca2b4765873307cb76620bbf544
KalebBKhayat <165746274+KalebBKhayat@users.noreply.github.com>
2025-12-02 22:24:23 +0000

    adiciona os scrip write e read do prettier para a formatacao do codgio

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>PUTZ GB pq vc fez isso?</h1>
+function Home() {
+  return <h1>PUTZ GB pq vc fez isso?</h1>;
+}
+
+function Teste() {
+  console.log("teste");
 }
 
 export default Home;
